2c119b6a4dae83b51346417b63718df6d28f289a
Alexander Rieß <alexander.riess@blateral.com>
2020-04-15 12:21:42 +0200

    Appending data-gtm attribute

--- src/modules/Action.tsx
+++ src/modules/Action.tsx
@@ -13,19 +13,26 @@ export const Action: React.FC<{
     buttonTag?: "a" | "button";
     onClick?: () => void;
     type?: "accept" | "decline";
+    gtmIdentifier?: string;
     className?: "string";
 }> = ({
     buttonTag: ButtonTag = "button",
     type,
     className = "",
     children,
-    onClick
+    onClick,
+    gtmIdentifier
 }) => (
     <ButtonTag
         onClick={onClick}
         className={`${styles.CookieConsent__action} ${
             type === "accept" ? styles["CookieConsent__action--accept"] : ""
         } ${className}`}
+        data-gtm={
+            gtmIdentifier || type === "accept"
+                ? "button-cookie-consent-accept"
+                : "button-cookie-consent-decline"
+        }
     >
         {children}
     </ButtonTag>
