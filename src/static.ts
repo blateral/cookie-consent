e26ca0dff2fac52085e631aa192435adf699b711
Lukas <strandlaefer@googlemail.com>
2020-04-21 16:44:31 +0200

    fixing on style change reload bug in webpack dev server

--- src/static.ts
+++ src/static.ts
@@ -1,3 +1,4 @@
+require("./styles.css");
 import {
     CookieConfigDefaults,
     CookieConfig,
--- webpack.config.js
+++ webpack.config.js
@@ -4,23 +4,31 @@ const path = require("path");
 module.exports = {
     mode: "production",
     entry: ["./src/static.ts"],
-    /*
-    watchOptions: {
-        ignored: ["node_modules/**", "dist/**"]
-    },
-    */
     output: {
         filename: "cookie-consent.min.js",
         path: path.resolve(__dirname, "example-static")
     },
     devServer: {
-        contentBase: "./example-static"
+        contentBase: "./example-static",
+        watchContentBase: true
     },
     module: {
         rules: [
             {
                 test: /\.ts?$/,
                 use: "ts-loader",
+                exclude: /node_modules/
+            },
+            {
+                test: /\.(jpe?g|gif|png|svg|woff|ttf|wav|mp3|css)$/,
+                use: [
+                    {
+                        loader: "file-loader",
+                        options: {
+                            name: "[name].[ext]"
+                        }
+                    }
+                ],
                 exclude: /node_modules/
             }
         ]
