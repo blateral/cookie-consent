01c855c4023927b86c074e3a7cc68dd6defedd91
Lukas <strandlaefer@googlemail.com>
2020-04-27 15:14:09 +0200

    improving date and time formatter util script

--- src/utils/formatter.ts
+++ src/utils/formatter.ts
@@ -1,5 +1,9 @@
 import { CookieConsentData } from "./cookie";
 
+interface DateReplacementHolder {
+    [key: string]: (date: Date) => string;
+}
+
 export const formatCookieStatusString = (
     cookie: CookieConsentData | undefined,
     str: string,
@@ -9,60 +13,109 @@ export const formatCookieStatusString = (
     const placeholders: {
         [key: string]: any;
     } = {
-        "%DATE%": cookie
-            ? getFormattedDateFromTimestamp(cookie.updatedAt, dateFormat)
-            : "",
-        "%TIME%": cookie
-            ? getFormattedTimeFromTimestamp(cookie.updatedAt, timeFormat)
-            : ""
+        "%DATE%": cookie ? getFormattedDate(cookie.updatedAt, dateFormat) : "",
+        "%TIME%": cookie ? getFormattedTime(cookie.updatedAt, timeFormat) : ""
     };
 
-    str = str.replace(/%\w+%/g, function(foundString) {
+    str = str.replace(/%\w+%/g, foundString => {
         return placeholders[foundString] || foundString;
     });
     return str;
 };
 
-const getFormattedDateFromTimestamp = (timestamp: number, format: string) => {
+const getFormattedDate = (timestamp: number, format: string) => {
     const date = new Date(timestamp);
-    format = format.replace(/(dd|DD|mm|MM|yy|YY|yyyy|YYYY)/g, foundString => {
-        switch (true) {
-            case foundString === "dd" || foundString === "DD":
-                return getDoubleDigitStringFromNumber(date.getDate());
-            case foundString === "mm" || "MM":
-                return getDoubleDigitStringFromNumber(date.getMonth() + 1);
-            case foundString === "yy" || foundString === "YY":
-                return date
-                    .getFullYear()
-                    .toString()
-                    .substr(-2);
-            case foundString === "yyyy" || foundString === "YYYY":
-                return date.getFullYear().toString();
-            default:
-                return "";
-        }
-    });
+
+    // replace placeholders with values from dateReplacements object
+    let dataKeys = Object.keys(dateReplacements);
+    for (let i = 0; i < dataKeys.length; i++) {
+        format = format.replace(new RegExp(dataKeys[i], "g"), foundString => {
+            return dateReplacements[foundString](date);
+        });
+    }
     return format;
 };
 
-const getFormattedTimeFromTimestamp = (timestamp: number, format: string) => {
+const getFormattedTime = (timestamp: number, format: string) => {
     const date = new Date(timestamp);
-    format = format.replace(/(hh|HH|mm|MM|ss|SS)/g, foundString => {
-        switch (true) {
-            case foundString === "hh" || foundString === "HH":
-                return getDoubleDigitStringFromNumber(date.getHours());
-            case foundString === "mm" || foundString === "MM":
-                return getDoubleDigitStringFromNumber(date.getMinutes());
 
-            case foundString === "ss" || foundString === "SS":
-                return getDoubleDigitStringFromNumber(date.getSeconds());
-            default:
-                return "";
-        }
-    });
+    // replace placeholders with values from timeReplacements object
+    let dataKeys = Object.keys(timeReplacements);
+    for (let i = 0; i < dataKeys.length; i++) {
+        format = format.replace(new RegExp(dataKeys[i], "g"), foundString => {
+            return timeReplacements[foundString](date);
+        });
+    }
     return format;
 };
 
-const getDoubleDigitStringFromNumber = (value: number) => {
+const dateReplacements: DateReplacementHolder = {
+    dd: (date: Date) => {
+        return getDay(date);
+    },
+    DD: (date: Date) => {
+        return getDay(date);
+    },
+    mm: (date: Date) => {
+        return getMonth(date);
+    },
+    MM: (date: Date) => {
+        return getMonth(date);
+    },
+    yy: (date: Date) => {
+        return getYear(date);
+    },
+    YY: (date: Date) => {
+        return getYear(date);
+    }
+};
+
+const timeReplacements: DateReplacementHolder = {
+    hh: (date: Date) => {
+        return getHours(date);
+    },
+    HH: (date: Date) => {
+        return getHours(date);
+    },
+    mm: (date: Date) => {
+        return getMinutes(date);
+    },
+    MM: (date: Date) => {
+        return getMinutes(date);
+    },
+    ss: (date: Date) => {
+        return getSeconds(date);
+    },
+    SS: (date: Date) => {
+        return getSeconds(date);
+    }
+};
+
+const getDoubleDigitString = (value: number) => {
     return value < 10 ? "0" + value.toString() : value.toString();
+};
+
+const getDay = (date: Date) => {
+    return getDoubleDigitString(date.getDate());
+};
+
+const getMonth = (date: Date) => {
+    return getDoubleDigitString(date.getMonth() + 1);
+};
+
+const getYear = (date: Date, full: boolean = false) => {
+    let year = date.getFullYear().toString();
+    return full ? year : year.substr(-2);
+};
+
+const getHours = (date: Date) => {
+    return getDoubleDigitString(date.getHours());
+};
+
+const getMinutes = (date: Date) => {
+    return getDoubleDigitString(date.getMinutes());
+};
+
+const getSeconds = (date: Date) => {
+    return getDoubleDigitString(date.getSeconds());
 };
